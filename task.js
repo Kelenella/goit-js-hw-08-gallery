296ba3ad55feb0f6e70a63ab83009dc0872e6945
Jane Yushchenko <kelenella@ukr.net>
2021-08-02 00:23:15 +0300

    done

--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './app.js';
+import {galleryItems} from './app.js';
 
 // Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
@@ -11,12 +11,12 @@ import { galleryItems } from './app.js';
 // Закрытие модального окна по нажатию клавиши ESC.
 // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
 
-const refs = {
-    createGalleryList: document.querySelector('.js-gallery'),
-    createModalImg: document.querySelector('.js-lightbox'),
-    lightboxImage: document.querySelector('.lightbox__image'),
-    closeBtn: document.querySelector('[data-action="close-lightbox"]'),
-    closeLightboxOverlay: document.querySelector('.lightbox__overlay'),
+const refs = { 
+createGalleryList: document.querySelector('.js-gallery'),
+createModalImg: document.querySelector('.js-lightbox'),
+lightboxImage: document.querySelector('.lightbox__image'),
+closeBtn: document.querySelector('[data-action="close-lightbox"]'),
+closeLightboxOverlay: document.querySelector('.lightbox__overlay'),
 }
 
 refs.createGalleryList.addEventListener('click', onImageClick);
@@ -26,8 +26,8 @@ refs.closeLightboxOverlay.addEventListener('click', closeModalWindow);
 //создание карточки галлереи
 const addGalleryEl = galleryItems.map(el => {
 
-    const addGalleryItem =
-        `<li class="gallery__item">
+    const addGalleryItem = 
+    `<li class="gallery__item">
   <a
     class="gallery__link"
     href= ${el.original}
@@ -41,7 +41,7 @@ const addGalleryEl = galleryItems.map(el => {
   </a>
 </li>`;
 
-    return addGalleryItem;
+return addGalleryItem;
 })
 refs.createGalleryList.insertAdjacentHTML("beforeend", addGalleryEl.join(''));
 
@@ -49,49 +49,49 @@ refs.createGalleryList.insertAdjacentHTML("beforeend", addGalleryEl.join(''));
 
 
 function onImageClick(event) {
-    if (event.target.nodeName === 'IMG') {
-        event.preventDefault();
-        refs.createModalImg.classList.add('is-open');// добавление класса в div
-        refs.lightboxImage.src = event.target.dataset.source;// получение url большого изображения.
-        refs.lightboxImage.alt = event.target.alt;
-        refs.lightboxImage.dataset.index = event.target.dataset.index;
-        // console.log(event.target.nodeName)
-    }
-    else {
-        return;
-    }
-    window.addEventListener('keydown', keyEscape);
+ if(event.target.nodeName === 'IMG') {
+     event.preventDefault();
+refs.createModalImg.classList.add('is-open');// добавление класса в div
+refs.lightboxImage.src = event.target.dataset.source;// получение url большого изображения.
+refs.lightboxImage.alt = event.target.alt;
+refs.lightboxImage.dataset.index = event.target.dataset.index;
+// console.log(event.target.nodeName)
+}
+else {
+  return;
+}
+window.addEventListener('keydown', keyEscape);
 }
 
 
 function keyEnterClick(event) {
-    if (event.code === "Enter" && event.target.nodeName === "A") {
-        refs.createModalImg.classList.add("is-open");
-        refs.lightboxImage.src = event.target.href;
-    }
-    window.addEventListener('keydown', keyEnterClick)
+  if (event.code === "Enter" && event.target.nodeName === "A") {
+    refs.createModalImg.classList.add("is-open");
+    refs.lightboxImage.src = event.target.href;
+} 
+window.addEventListener('keydown', keyEnterClick)
 }
 
 function closeModalWindow() {
     refs.createModalImg.classList.remove('is-open');
-    refs.lightboxImage.src = "#";
-    refs.lightboxImage.alt = "#";
+    refs.lightboxImage.src ="#";
+    refs.lightboxImage.alt ="#";
 
     // window.removeEventListener('keydown', keyEscape);
-}
+ }
 
 function keyEscape(event) {
-    if (event.code === "Escape") {
+    if(event.code === "Escape") {
         closeModalWindow()
     }
-
+    
 }
 
 window.addEventListener("keydown", (event) => {
-    if (event.code === "ArrowLeft") {
-        onArrowLeft();
-    }
-    if (event.code === "ArrowRight") {
-        onArrowRight();
-    }
+  if (event.code === "ArrowLeft") {
+    onArrowLeft();
+  }
+  if (event.code === "ArrowRight") {
+    onArrowRight();
+  }
 });
